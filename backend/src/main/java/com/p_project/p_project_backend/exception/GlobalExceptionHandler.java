edcba8eaf68d99ee8c7a90f188ceb3828f948dd0
junhyeok020630 <skylove2424@gmail.com>
2025-12-03 16:21:07 +0900

    [refactor] 프로젝트 폴더 명 수정 + 백엔드 폴더 사용자/관리자 분할

---
+++ backend/src/main/java/com/p_project/p_project_backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.p_project.p_project_backend.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+        return ResponseEntity.badRequest().body(
+                Map.of("success", false, "error", Map.of("code", "VALIDATION_FAILED", "message", errorMessage)));
+    }
+}
